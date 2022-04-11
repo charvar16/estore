7b897c2d2a927cef2446cf6d8cf0d084f18355ed
chad589k <proton74955ilo@protonmail.com>
2022-04-11 13:40:27 -0400

    todo fino

--- src/routes/collections.js
+++ src/routes/collections.js
@@ -2,8 +2,38 @@ import { Outlet } from 'react-router-dom'
 
 export default function Collections() {
     return (
-      <>
-        <p>Collections</p>
-      </>
+      <div className="prod-container">
+        <img className="prod-img" src="https://cdn.shopify.com/s/files/1/0247/8869/7150/products/STACKER_360x.png?v=1647439443" alt="Error" /> 
+
+        <div className="right">
+          <p className="title">COMBO RODEO STACKER</p>
+          <p className="price">$12.99</p>
+
+          <form action="/action_page.php">
+            <p className="label-size prod-labels">TAMAÑO</p>
+            <select class="prod-select" value="MEDIANO"> 
+                    <option value="mediano">MEDIANO</option>
+                    <option value="grande">GRANDE</option>
+                    <option value="king">KING</option>
+            </select>
+            <p className="label-extra prod-labels">CON EXTRA</p>
+            <select class="prod-select" value="SIN EXTRA"> 
+                    <option value="sin extra">SIN EXTRA</option>
+                    <option value="queso">QUESO</option>
+                    <option value="queso y tocineta">QUESO Y TOCINETA</option>
+            </select>
+
+            <p className="label-acompanantes prod-labels">ACOMPAÑANTES</p>
+            <select class="prod-select" value="papas"> 
+                    <option value="papas">PAPAS</option>
+                    <option value="aros de cebolla">AROS DE CEBOLLA</option>
+            </select>
+
+            <button className="carrito prod-bttn prod-select">AGREGAR AL CARRITO</button>
+            <button className="comprar prod-bttn prod-select">COMPRAR AHORA</button>
+            <p className="description">Disfruta de la Rodeo Stacker y vive una experiencia impactante con los crujientes aros de cebolla que son los protagonistas de esta deliciosa hamburguesa, acompañados de salsa Stacker, salsa BBQ, queso cheddar, tocineta y dos jugosas carnes a la parrilla.</p>
+          </form>
+        </div>
+      </div>
     );
   }
--- src/routes/search.js
+++ src/routes/search.js
@@ -1,12 +1,21 @@
-import ComboGroup from '../components/combogroup'
+import Combo from '../components/combo'
 
 export default function Search() {
     return (
-      <>
+      <div className="search">
+        <div className="search-terms">
         <p>Su búsqueda de "whopper" muestra lo siguiente:</p>
-        <div className="search-results">
-            <ComboGroup />
+        <input className="search-results-bar" type="search"/>
         </div>
-      </>
+        <div className="search-results">
+            <Combo />
+            <Combo />
+            <Combo />
+            <Combo />
+            <Combo />
+            <Combo />
+            <Combo />
+        </div>
+      </div>
     );
   }
