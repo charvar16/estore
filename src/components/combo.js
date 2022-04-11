f347355649396d30f47e9748712a8e473c2f2f18
chad589k <proton74955ilo@protonmail.com>
2022-04-11 08:45:06 -0400

    changes so far

--- src/components/combo.js
+++ src/components/combo.js
@@ -6,7 +6,8 @@ export default function Combo() {
             <Link to="collections/bundles-familiares">
                 <img className="combo-img" src="https://cdn.shopify.com/s/files/1/0247/8869/7150/products/BUNDLEDELREY400x400_360x.png?v=1631287322" alt="error"/>
                 <p className="combo-name">BUNDLE DEL REY</p>
-                <span className="combo-price">$24.50</span>                
+                <span className="combo-price">$24.50</span>
+                <button className="combo-add" type="submit">Agregar</button>
             </Link>
         </span>
     );
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,9 @@
 export default function Footer() {
     return (
         <div id="footer">
-            <div className="wrapper">
               <div id="footer-container">
-                <div id="social-media-accounts">
-                  <div id="social-media-accounts-container">
-                  </div>
-                </div>
+                <p>© 2022 BURGER KING VENEZUELA</p>
               </div>
-            </div>
         </div>
     );
   }
